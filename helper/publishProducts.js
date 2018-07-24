77b37a9212df072475ae9b589fc979d2638c6d27
Mohit Jain 10 <mjain30@sapient.com>
2018-07-24 17:39:57 +0100

    review comments incorporated

--- helper/publishProducts.js
+++ helper/publishProducts.js
@@ -17,61 +17,58 @@ const catalogs = require(`${configFolderPath}/catalogs.json`);
 function publishProduct() {
     for (let catalog in catalogs) {
         if (!Array.isArray(catalogs[catalog])) {
-            
-            for (let space in catalogs[catalog]) {
-                if (shell.exec(`apic config:set space=apic-space://${apicServer}/orgs/${apicOrg}/catalogs/${catalog}/spaces/${space}`).code === 0) {
-                    const catalogSpace = catalogs[catalog][space];
-                    publishProductWithSpace(catalogSpace, apicServer , apicOrg );
-
-                } else {
-                    logger(`Error: setting space to ${space} in catalog ${catalog} and organisation - ${apicOrg}`);
-                    return shell.exit(1);
-                }
-            }
+            publishProductWithSpace(catalog, apicServer, apicOrg);
         } else {
-            if (shell.exec(`apic config:set catalog=apic-catalog://${apicServer}/orgs/${apicOrg}/catalogs/${catalog}`).code === 0) {
-                const catalog = catalogs[catalog];
-                publishProductWithoutSpace(catalog, apicServer , apicOrg);
-                
-            } else {
-                logger(`Error: setting catalog to ${catalog} in organisation - ${apicOrg}`);
-                return shell.exit(1);
-            }
+            publishProductWithoutSpace(catalog, apicServer, apicOrg);
         }
     }
 };
 
-function publishProductWithSpace(products, apicServer , apicOrg ) {
-    for (let obj of products) {
-        let product = obj.productName,
-            isDeploy = obj.deploy || false;
-        if (isDeploy === true) {
-            logger(`publishing product ${product} started`);
-            if (shell.exec(`apic publish -s ${apicServer} ${product} --scope space`).code !== 0) {
-                logger(`Error: publishing product ${product}`);
-                shell.exec(`cat ${product}`);
-                return shell.exit(1);
+function publishProductWithSpace(catalog, apicServer, apicOrg) {
+    for (let space in catalogs[catalog]) {
+        if (shell.exec(`apic config:set space=apic-space://${apicServer}/orgs/${apicOrg}/catalogs/${catalog}/spaces/${space}`).code === 0) {
+            const products = catalogs[catalog][space];
+            for (let obj of products) {
+                let product = obj.productName,
+                    isDeploy = obj.deploy || false;
+                if (isDeploy === true) {
+                    logger(`publishing product ${product} started`);
+                    if (shell.exec(`apic publish -s ${apicServer} ${product} --scope space`).code !== 0) {
+                        logger(`Error: publishing product ${product}`);
+                        shell.exec(`cat ${product}`);
+                        return shell.exit(1);
+                    }
+                    logger(`publishing product ${product} finished`);
+                    shell.exec(`sleep ${tts}`);
+                }
             }
-            logger(`publishing product ${product} finished`);
-            shell.exec(`sleep ${tts}`);
-        } 
+        } else {
+            logger(`Error: setting space to ${space} in catalog ${catalog} and organisation - ${apicOrg}`);
+            return shell.exit(1);
+        }
     }
 }
-function publishProductWithoutSpace(products, apicServer , apicOrg) {
-    for (let obj of products) {
-        let product = obj.productName,
-            isDeploy = obj.deploy || false;
-        if (isDeploy === true) {
-            logger(`publishing product ${product} started`);
-            if (shell.exec(`apic publish -s ${apicServer} ${product}`).code !== 0) {
-                logger(`Error: publishing product ${product}`);
-                shell.exec(`cat ${product}`);
-                return shell.exit(1);
+
+function publishProductWithoutSpace(catalog, apicServer, apicOrg) {
+    if (shell.exec(`apic config:set catalog=apic-catalog://${apicServer}/orgs/${apicOrg}/catalogs/${catalog}`).code === 0) {
+        for (let obj of catalogs[catalog]) {
+            let product = obj.productName,
+                isDeploy = obj.deploy || false;
+            if (isDeploy === true) {
+                logger(`publishing product ${product} started`);
+                if (shell.exec(`apic publish -s ${apicServer} ${product}`).code !== 0) {
+                    logger(`Error: publishing product ${product}`);
+                    shell.exec(`cat ${product}`);
+                    return shell.exit(1);
+                }
+                logger(`publishing product ${product} finished`);
+                shell.exec(`sleep ${tts}`);
+                console.log("inside true", scope);
             }
-            logger(`publishing product ${product} finished`);
-            shell.exec(`sleep ${tts}`);
-            console.log("inside true" , scope);
-        } 
+        }
+    } else {
+        logger(`Error: setting catalog to ${catalog} in organisation - ${apicOrg}`);
+        return shell.exit(1);
     }
 }
 module.exports = {
