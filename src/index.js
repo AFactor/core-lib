83af45e3cb0e790c6e1e06fccec803482ce5a01a
Mohit Jain 10 <mjain30@sapient.com>
2018-07-25 11:00:06 +0100

    All the unncessary dependencies removed from index.js

--- src/index.js
+++ src/index.js
@@ -1,21 +1,9 @@
-/* eslint-disable strict, max-len */
-const fs = require('fs');
-const path = require('path');
-const fsExtra = require('fs-extra');
-let argv = require('yargs').argv;
-const util = require('util');
-const yaml = require('js-yaml');
-const handlebars = require('handlebars');
-const chalk = require('chalk');
-const encoding = 'utf-8';
-const writeFile = require('write');
 
 //Importing all the functions from helper folder 
 const { replaceTokens , replaceTokenValue , handleErrors, findUnreplacedTokens, replaceConfigTokens, findTokenDelimiters } = require('../helper/replaceTokens.js');
 const { setupApis, loadDynamicConf, createApiFile } = require('../helper/setupApis.js');
 const { setupProducts, replaceLiteralTokens, transformer, createProductFiles, convertToYAML } = require('../helper/setupProducts.js');
 const { publishProduct , publishProductWithSpace , publishProductWithoutSpace } = require('../helper/publishProducts.js');
-
 
 
 // Setup --- for creating definitions folder containing the yaml 
